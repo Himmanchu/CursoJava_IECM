bdfc2d80e06bd865312f58287aae6dac5890c910
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-04-28 12:58:05 +0200

    Commit

--- src/Factura.java
+++ src/Factura.java
@@ -1,26 +1,29 @@
-
+import javax.swing.*;
 public class Factura {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int Factura;
 		String NombreTitular;
 		String ConceptoFactura;
 		double Precio;
 		double Total;
 		double Iva;
 		
+		
+		Factura=Integer.parseInt(JOptionPane.showInputDialog("Introduce nº de Factura"));
 		NombreTitular= "Javier Jiménez Sanz";
 		ConceptoFactura= "Realización Programa tv";
 		Precio= 890;
 		Iva= Precio * 0.21;
 		Total= Precio + Iva;
 		
-		System.out.println("Factura de " + NombreTitular);
+		System.out.println("Factura nº" + Factura + " de " + NombreTitular);
 		System.out.println("Concepto de la factura: "+ ConceptoFactura);
-		System.out.println(Precio + " € ");
-		System.out.println(Iva + " € ");
-		System.out.println(Total + " € ");
+		System.out.println("Precio " + Precio + " € ");
+		System.out.println("Iva " + Iva + " € ");
+		System.out.println("Total " + Total + " € ");
 		
 		
 		
